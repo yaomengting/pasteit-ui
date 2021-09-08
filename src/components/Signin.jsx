31651179876211fbb9ae91ed6b6bab4f0b94800c
yaomengting <yaomengting110@gmail.com>
2021-09-08 10:56:45 -0500

    feat: edit Signin component

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function Signin(){
+
+    const[data, setData] = useState(){
+        email = ""
+        password = ""
+
+    }
+
+    setData
+
+    function handleClick(){
+        
+           console.log("i am l");
+        
+    }
     return (
         <div className="container"> 
             <form className="form">
                 <input type="text" placeholder="Email" />
                 <input type="password" placeholder="Password" />
-                <button type="submit">Signin</button>
+                <button onClick={handleClick} type="submit">Signin</button>
             </form>
         </div>
     )
