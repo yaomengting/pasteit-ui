5bc3f1bec9c4a27ebc571c8ac406139c0f015285
yaomengting <yaomengting110@gmail.com>
2021-09-08 10:56:21 -0500

    feat: edit Paste component

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,39 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Paste(){
-    const[paste, setPaste] = useState({
-        content:""
-    });
-
-    function handleChange(event){
-        const{name, value} = event.target;
-        setPaste(prevNote =>{
-            return{
-                ...prevNote,
-                [name]: value
-
-            };
-        });
+function Paste(props){
+    function handleClick(){
+        props.onDelete(props.id);
     }
 
-    function submitPaste(){
-
-    }
     return (
-        <div >
-            <h1>New Paste</h1>
-            <textarea 
-                name="content" 
-                value={paste.content} 
-                onChange={handleChange}
-                placeholder="Paste here..." 
-                rows="20">paste here</textarea>
-            <button onClick={submitPaste}>Create a paste</button>
+        <div>
+            <p>{props.content}</p>
+            <button onClick={handleClick}>DELETE</button>
         </div>
 
-    )
-    
-    ;
+    );
 }
 
-export default Paste;
+export default Paste
