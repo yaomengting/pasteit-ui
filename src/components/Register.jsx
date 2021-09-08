c9a300ba0813eb0f2d7a6e88e0f6c61d98246094
yaomengting <yaomengting110@gmail.com>
2021-09-08 14:18:53 -0500

    feat: add click event handler

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function Register(){
+    const [data, setData] = useState({
+        username:"",
+        email: "",
+        password: "",
+        comfirmPassword: ""
+    })
+
+    function handleClick() {
+        console.log("i am l");
+        console.log(data);
+    }
+
+    function updateUserNameText(event) {
+        console.log('username:', event.target.value)
+        setData(prevPaste => {
+            return {
+                ...prevPaste,
+                username: event.target.value
+            };
+        });
+    }
+
+    function updateEmailText(event) {
+        console.log('email:', event.target.value)
+        setData(prevPaste => {
+            return {
+                ...prevPaste,
+                email: event.target.value
+            };
+        });
+    }
+
+    function updatePwdText(event) {
+        console.log('pwd:', event.target.value);
+        setData(prevPaste => {
+            return {
+                ...prevPaste,
+                password: event.target.value
+            };
+        });
+    }
+
+    function updateComfirmPwdText(event) {
+        console.log('confirmpwd:', event.target.value);
+        setData(prevPaste => {
+            return {
+                ...prevPaste,
+                comfirmPassword: event.target.value
+            };
+        });
+    }
     return (
         <div className="container"> 
-            <form className="form">
-                <input type="text" placeholder="Username" />
-                <input type="text" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input type="password" placeholder="Confirm Password" />
-                <button type="submit">Register</button>
-            </form>
+                <input type="text" onChange={updateUserNameText} placeholder="Username" />
+                <input type="text" onChange={updateEmailText} placeholder="Email" />
+                <input type="password" onChange={updatePwdText} placeholder="Password" />
+                <input type="password" onChange={updateComfirmPwdText} placeholder="Confirm Password" />
+                <button onClick={handleClick} type="submit">Register</button>
+                <br />
+                <label>Already a user of Pasteit? -> </label>
+            <a href="./signin">Signin</a>
         </div>
     )
     
