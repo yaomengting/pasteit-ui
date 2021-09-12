6b9325b9f401409858b3d826239d64fa02875a5a
yaomengting <yaomengting110@gmail.com>
2021-09-11 20:06:45 -0500

    update: createpaste can get access to uniqueUrl of each paste

--- src/services/paste.js
+++ src/services/paste.js
@@ -1,23 +1,21 @@
 const axios = require('axios');
 const token = require('./token');
 const url = require('./url');
-
 async function createPaste(paste) {
-    
+
     const url = "http://localhost:8088/pasteit-services/v1/paste";
     const config = {
-        headers:{
-            Authorization: token.get
-        }
+        headers: { 'Authorization': localStorage.getItem('access_token') }
     }
-    try{
-        const res = await axios.post(url, paste,config);
-        if(res.status == 201){
-            // to do: first print res, and find the paste.url, second, set it;
-            url.set("res.paste.url");
+    try {
+        const res = await axios.post(url, paste, config);
+        console.log("res", res)
+        if (res.status == 201) {
+            const uniqueUrl = res.data.success.data.paste.url;
+            localStorage.setItem('unique_url', uniqueUrl);
             return true;
         }
-    }catch(error){
+    } catch (error) {
         console.log(JSON.stringify(error));
     }
     return false;
@@ -27,7 +25,7 @@ async function createPaste(paste) {
 async function getAllPaste(userId) {
 
     // to do: getallpaste by userid 
-    
+
     /*
     const url = "http://localhost:8088/pasteit-services/v1/paste";
     const config = {
@@ -46,12 +44,28 @@ async function getAllPaste(userId) {
     return false;
     */
 }
+const pasteUrl = "http://localhost:8088/pasteit-services/v1/paste/" + localStorage.getItem('unique_url');
 async function getOnePaste(pasteUrl) {
-    // first, get uniqueUrl 
-    const uniqueUrl = url.get();
-   // second, add it after the url;
-   const url = "http://localhost:8088/pasteit-services/v1/paste" + "/uniqueUrl";
-
+    const config = {
+        headers: { 'Authorization': localStorage.getItem('access_token') }
+    }
+    try {
+        const res = await axios.get(pasteUrl, config);
+        if (res.status == 201) {
+            return true;
+        }
+    } catch (error) {
+        console.log(JSON.stringify(error));
+    }
+    return false;
 }
 
-module.exports = { createPaste, getAllPaste, getOnePaste};
+module.exports = { createPaste, getAllPaste, getOnePaste };
+
+(async () => {
+    return
+    const paste = {
+        content: "ddd"
+    }
+    await createPaste(paste);
+})()
